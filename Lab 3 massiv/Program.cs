96f95959255544504b5114c7c858147e2d844033
Михаил Семакин < >
2020-10-14 16:55:39 +0300

    Добавьте файлы проекта.

---
+++ Lab 3 massiv/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_massiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int elementsCount = Convert.ToInt32(Console.ReadLine());
+            int[] myArray = new int [elementsCount];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"Введите элемент массива под индексом {i}: ");
+                myArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Вывод массива: ");
+            for(int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
